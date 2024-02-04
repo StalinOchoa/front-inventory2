03d3203df047a0705a13a613c81623f689f87d0e
StalinOchoa <stalin.or7@live.com>
2024-02-04 17:28:06 -0500

    primera version de front de inventario

---
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard.component';
+//archivo para rutas hijas asociado a este modulo dashboardRoutingModule
+const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: DashboardComponent, 
+        loadChildren: () => import('./router-child.modules').then(m => m.RouterChildModule)
+    },
+]
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+    
+})
+export class DashboardRoutingModule { }
